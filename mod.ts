04b638326ae2a7730f8ad85ff8b738402ee81815
urielch <uriel.chemouni@gmail.com>
2025-03-04 12:31:02 +0100

    regen

--- mod.ts
+++ mod.ts
@@ -1575,7 +1575,7 @@ export function FiTv(props: IconBaseProps): VNode<JSX.SVGAttributes> {
  * Twitch icon from Feather
  */
 export function FiTwitch(props: IconBaseProps): VNode<JSX.SVGAttributes> {
-  return GenIcon({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M21 2H3v16h5v4l4-4h5l4-4V2zM11 11V7M16 11V7"}}]})(props);
+  return GenIcon({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7"}}]})(props);
 }
 /**
  * Twitter icon from Feather
